68f37cdf66a804124b11ba2ec0091e89e2de27bd
bowlanger <bowlanger.git@gmail.com>
2020-04-01 21:12:21 +0200

    split method GetCarTrackH2Node into specialized functions

--- SetupExplorerLibrary/SetupParser.cs
+++ SetupExplorerLibrary/SetupParser.cs
@@ -14,22 +14,31 @@ namespace SetupExplorerLibrary
 
         private HtmlDocument doc = new HtmlDocument();
         private HtmlNodeCollection H2Nodes;
+        private HtmlNode SetupSummaryH2Node;
 
         // Test
         public string h2cartrack;
 
-        private string Car;
+        private string CarName;
         private string SetupName;
-        private string Track;
+        private string TrackName;
 
         public SetupParser(string InputFile)
         {
             this.InputFile = InputFile;
 
             LoadInputFile();
-
             H2Nodes = GetH2Nodes();
-            GetCarTrackH2Node();
+
+            SetupSummaryH2Node = GetSetupSummaryH2Node();
+            CarName = GetSanitizedCarName(SetupSummaryH2Node.ChildNodes[2].InnerText.Trim());
+            SetupName = GetSanitizedSetupName(SetupSummaryH2Node.ChildNodes[2].InnerText.Trim());
+            TrackName = GetSanitizedTrackName(SetupSummaryH2Node.ChildNodes[4].InnerText.Trim());
+
+
+            Console.WriteLine("carname : " + CarName);
+            Console.WriteLine("trackname : " + TrackName);
+            Console.WriteLine("setupname : " + SetupName);
 
             // Test
             h2cartrack = doc.DocumentNode.SelectSingleNode("//h2[@align=\"center\"]").InnerHtml;
@@ -42,27 +51,49 @@ namespace SetupExplorerLibrary
 
         private HtmlNodeCollection GetH2Nodes()
         {
-            Console.WriteLine("GetH2Nodes");
-            HtmlNodeCollection H2Nodes = doc.DocumentNode.SelectNodes("/html/body/h2");
+            HtmlNodeCollection H2Nodes = doc.DocumentNode.SelectNodes("//h2");
 
             return H2Nodes;
         }
 
-        // retrieve and format from first H2 element : car, track and setup names
-        private void GetCarTrackH2Node()
+        private HtmlNode GetSetupSummaryH2Node()
         {
-            Console.WriteLine("GetCarTrackH2Node");
-            HtmlNode cartrackH2Node = H2Nodes.First();
-            string carsetup = cartrackH2Node.ChildNodes[2].InnerText.Trim();
-            string track = cartrackH2Node.ChildNodes[4].InnerText.Trim();
-            Car = carsetup.Substring(0, carsetup.IndexOf(":") - 5);
-            Track = track.Substring(track.IndexOf(":") + 2);
-            SetupName = carsetup.Substring(carsetup.IndexOf(":") + 2);
-
-            Console.WriteLine("car : " + Car);
-            Console.WriteLine("track : " + Track);
-            Console.WriteLine("setup : " + SetupName);
+            return H2Nodes.First();
         }
+
+        private string GetSanitizedCarName(string carsetupLine)
+        {
+            // get substring from carsetupLine until ":" and get rid of trailing string "setup"
+            return carsetupLine.Substring(0, carsetupLine.IndexOf(":") - 5);
+        }
+
+        private string GetSanitizedSetupName(string carsetupLine)
+        {
+            // get substring from carsetupLine starting at ":" and offset by "+ 2" to the actual beginning of the setup name
+            return carsetupLine.Substring(carsetupLine.IndexOf(":") + 2);
+        }
+
+        private string GetSanitizedTrackName(string trackLine)
+        {
+            // get substring from trackLine starting at ":" and offset by "+ 2" to the actual beginning of the track name
+            return trackLine.Substring(trackLine.IndexOf(":") + 2);
+        }
+
+        // retrieve and format from first H2 element : car, track and setup names
+        //private void GetCarTrackH2Node()
+        //{
+        //    Console.WriteLine("GetCarTrackH2Node");
+        //    HtmlNode cartrackH2Node = H2Nodes.First();
+        //    string carsetup = cartrackH2Node.ChildNodes[2].InnerText.Trim();
+        //    string track = cartrackH2Node.ChildNodes[4].InnerText.Trim();
+        //    Car = carsetup.Substring(0, carsetup.IndexOf(":") - 5);
+        //    Track = track.Substring(track.IndexOf(":") + 2);
+        //    SetupName = carsetup.Substring(carsetup.IndexOf(":") + 2);
+
+        //    Console.WriteLine("car : " + Car);
+        //    Console.WriteLine("track : " + Track);
+        //    Console.WriteLine("setup : " + SetupName);
+        //}
 
     }
 }
