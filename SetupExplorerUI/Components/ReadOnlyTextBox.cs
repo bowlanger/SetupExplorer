71f22e4ecbf6d3862ba5389257ffb831457d0e12
bowlanger <bowlanger.git@gmail.com>
2020-04-04 15:09:15 +0200

    ReadOnlyTextBox
    Implémentation de la ReadOnlyTextBox like a bg

---
+++ SetupExplorerUI/Components/ReadOnlyTextBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SetupExplorerUI.Components
+{
+    public class ReadOnlyTextBox : TextBox
+    {
+        [DllImport("user32.dll")]
+        static extern bool HideCaret(IntPtr hWnd);
+
+        public ReadOnlyTextBox()
+        {
+            this.ReadOnly = true;
+            //this.BackColor = Color.White;
+            this.GotFocus += TextBoxGotFocus;
+            //this.Cursor = Cursors.Arrow; // mouse cursor like in other controls
+        }
+
+        private void TextBoxGotFocus(object sender, EventArgs args)
+        {
+            HideCaret(this.Handle);
+        }
+    }
+}
